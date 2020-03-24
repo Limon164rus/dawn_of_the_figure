cfc5e18df00eef60fd72d9e980cc27f59ed4bbc6
Limon164rus <62592358+Limon164rus@users.noreply.github.com>
2020-03-24 13:00:15 +0400

    Add files via upload

---
+++ figure.py
@@ -0,0 +1,17 @@
+import math
+a=str(input('Введите название фигуры:'))
+if a=='Треугольник':
+    a=float(input('Введите сторону a:'))
+    b=float(input('Введите сторону b:'))
+    c=float(input('Введите сторону c:'))
+    P=(a+b+c)/2
+    print(P)
+elif a=='Прямоугольник':
+    a=float(input('Введите сторону a:'))
+    b=float(input('Введите сторону b:'))
+    S=a*b
+    print(S)
+elif a=='Круг':
+    r=float(input('Введите радиус:'))
+    S=math.pi*pow(r,2)
+    print(S)
